ba9bd2aa7f1a1e03ecf08073b6aa52b572177787
kuehkookies <ryankaskuser@gmail.com>
2014-10-29 10:15:22 +0700

    Implementing basic blogging feature

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,7 +1,9 @@
 class ArticlesController < ApplicationController
 	def index
-		@articles = Articles.all
+		@articles = Article.all
 	end
 
-	def show;end
+	def show
+		@article = Article.find(params[:id])
+	end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,10 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20141028130216) do
+ActiveRecord::Schema.define(:version => 20141029023838) do
 
   create_table "articles", :force => true do |t|
-    t.string   "name"
+    t.string   "title"
     t.text     "body"
     t.datetime "created_at", :null => false
     t.datetime "updated_at", :null => false
